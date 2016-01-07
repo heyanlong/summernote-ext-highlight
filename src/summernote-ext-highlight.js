769b923fbc85b48761c4677e4c41098afde5adc8
heyanlong <heyanlong@kaiyuan.com>
2016-01-07 14:13:57 +0800

    fix #3

--- src/summernote-ext-highlight.js
+++ src/summernote-ext-highlight.js
@@ -1,6 +1,6 @@
 /*!
  * summernote highlight plugin
- * http://www.heyanlong.com/
+ * http://www.hyl.pw/
  *
  * Released under the MIT license
  */
@@ -9,230 +9,183 @@
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = factory(require('jquery'));
     } else {
         // Browser globals: jQuery
         factory(window.jQuery);
     }
 }(function ($) {
-    // template
-    var tmpl = $.summernote.renderer.getTemplate();
 
-    // core functions: range, dom
-    var range = $.summernote.core.range;
-    var dom = $.summernote.core.dom;
-
-    /**
-     * createCodeNode
-     *
-     * @member plugin.highlight
-     * @private
-     * @param {String} code
-     * @return {Node}
-     */
-    var createCodeNode = function (code, select) {
-        var $code = $('<code>');
-        $code.html(code);
-        $code.addClass('language-' + select);
-
-        var $pre = $('<pre>');
-        $pre.html($code)
-        $pre.addClass('prettyprint').addClass('linenums');
-
-        return $pre[0];
-    };
-
-    var getTextOnRange = function ($editable) {
-        $editable.focus();
-
-        var rng = range.create();
-
-        // if range on anchor, expand range with anchor
-        if (rng.isOnAnchor()) {
-            var anchor = dom.ancestor(rng.sc, dom.isAnchor);
-            rng = range.createFromNode(anchor);
-        }
-
-        return rng.toString();
-    };
-
-    var toggleBtn = function ($btn, isEnable) {
-        $btn.toggleClass('disabled', !isEnable);
-        $btn.attr('disabled', !isEnable);
-    };
-
-    var showHighlightDialog = function ($editable, $dialog, text) {
-        return $.Deferred(function (deferred) {
-            var $highlightDialog = $dialog.find('.note-highlight-dialog');
-
-            var $highlightCode = $highlightDialog.find('.note-highlight-code'),
-                $highlightBtn = $highlightDialog.find('.note-highlight-btn'),
-                $highlightSelect = $highlightDialog.find('.note-highlight-select');
-
-            $highlightDialog.one('shown.bs.modal', function () {
-                $highlightCode.val(text).on('input', function () {
-                    toggleBtn($highlightBtn, $highlightCode.val());
-                }).trigger('focus');
-
-                $highlightBtn.click(function (event) {
-                    event.preventDefault();
-                    deferred.resolve($highlightCode.val(), $highlightSelect.val());
-                    $highlightDialog.modal('hide');
-                });
-            }).one('hidden.bs.modal', function () {
-                $highlightCode.off('input');
-                $highlightBtn.off('click');
-
-                if (deferred.state() === 'pending') {
-                    deferred.reject();
-                }
-            }).modal('show');
-        });
-    };
-
-    /**
-     * @class plugin.highlight
-     *
-     * Code Highlight Plugin
-     */
-    $.summernote.addPlugin({
-        /** @property {String} name name of plugin */
-        name: 'highlight',
-        /**
-         * @property {Object} buttons
-         * @property {Function} buttons.highlight   function to make button
-         */
-        buttons: { // buttons
-            highlight: function (lang) {
-                return tmpl.iconButton('fa fa-file-code-o', {
-                    event: 'highlight',
-                    title: 'highlight',
-                    title: lang.highlight.highlight,
-                    hide: true
-                });
-            }
-        },
-        /**
-         * @property {Object} dialogs
-         * @property {function(object, object): string} dialogs.highlight
-         */
-        dialogs: {
-            highlight: function (lang) {
-                var body = '<div class="form-group">' +
-                    '<select class="form-control note-highlight-select">' +
-                    '<option value="bsh">bsh</option>' +
-                    '<option value="c">c</option>' +
-                    '<option value="cc">cc</option>' +
-                    '<option value="cpp">cpp</option>' +
-                    '<option value="cs">cs</option>' +
-                    '<option value="csh">csh</option>' +
-                    '<option value="cyc">cyc</option>' +
-                    '<option value="cv">cv</option>' +
-                    '<option value="htm">htm</option>' +
-                    '<option value="html">html</option>' +
-                    '<option value="java">java</option>' +
-                    '<option value="js">js</option>' +
-                    '<option value="m">m</option>' +
-                    '<option value="mxml">mxml</option>' +
-                    '<option value="perl">perl</option>' +
-                    '<option value="pl">pl</option>' +
-                    '<option value="pm">pm</option>' +
-                    '<option value="py">py</option>' +
-                    '<option value="php">php</option>' +
-                    '<option value="rb">rb</option>' +
-                    '<option value="sh">sh</option>' +
-                    '<option value="xhtml">xhtml</option>' +
-                    '<option value="xml">xml</option>' +
-                    '<option value="xsl">xsl</option>' +
-                    '</select>' +
-                    '</div>' +
-                    '<div class="form-group">' +
-                    '<label>' + lang.highlight.codeLabel + '</label>' +
-                    '<textarea class="note-highlight-code form-control" rows="10"></textarea>' +
-                    '</div>';
-                var footer = '<button href="#" class="btn btn-primary note-highlight-btn disabled" disabled>' + lang.highlight.insert + '</button>';
-                return tmpl.dialog('note-highlight-dialog', lang.highlight.insert, body, footer);
-            }
-        },
+    // Extends plugins for adding highlight.
+    //  - plugin is external module for customizing.
+    $.extend($.summernote.plugins, {
 
         /**
-         * @property {Object} events
-         * @property {Function} events.highlight  run function when button that has a 'highlight' event name  fires click
+         * @param {Object} context - context object has status of editor.
          */
-        events: { // events
-            highlight: function (event, editor, layoutInfo) {
-                var $dialog = layoutInfo.dialog(),
-                    $editable = layoutInfo.editable(),
-                    text = getTextOnRange($editable);
+        'highlight': function (context) {
 
-                editor.saveRange($editable);
-                showHighlightDialog($editable, $dialog, text).then(function (code, select) {
-                    // when ok button clicked
+            var self = this;
 
-                    // restore range
-                    editor.restoreRange($editable);
+            var ui = $.summernote.ui;
+            var $editor = context.layoutInfo.editor;
+            var options = context.options;
+            var lang = options.langInfo;
 
-                    // build node
-                    var $node = createCodeNode(code, select);
 
-                    if ($node) {
-                        // insert code node
-                        editor.insertNode($editable, $node);
+            // add button
+
+            context.memo('button.highlight', function () {
+                // create button
+                var button = ui.button({
+                    contents: '<i class="fa fa-file-code-o"></i>',
+                    tooltip: 'highlight',
+                    click: function () {
+                        self.show()
                     }
-                    //prettyPrint();
-                }).fail(function () {
-                    // when cancel button clicked
-                    editor.restoreRange($editable);
                 });
-            }
-        },
-        // define language
-        langs: {
-            'en-US': {
-                highlight: {
-                    highlight: 'Insert code',
-                    insert: 'Insert code',
-                    codeLabel: 'Enter the code fragment'
+
+                // create jQuery object from button instance.
+                var $highlight = button.render();
+                return $highlight;
+            });
+
+            this.createDialog = function () {
+
+                var $box = $('<div />');
+                var $selectGroup = $('<div class="form-group" />');
+                var $textGroup = $('<div class="form-group" />');
+                var $select = $('<select class="form-control ext-highlight-select" />');
+
+                var languages = [
+                    'bsh', 'c', 'cc', 'cpp', 'cs', 'csh', 'cyc', 'cv', 'htm', 'html',
+                    'java', 'js', 'm', 'mxml', 'perl', 'pl', 'pm', 'py', 'php', 'rb',
+                    'sh', 'xhtml', 'xml', 'xsl'
+                ];
+
+                for (var i = 0; i < languages.length; i++) {
+                    $select.append('<option value="' + languages[i] + '">' + languages[i] + '</option>');
                 }
-            },
-            'ar-AR': {},
-            'ca-ES': {},
-            'cs-CZ': {},
-            'da-DK': {},
-            'de-DE': {},
-            'es-ES': {},
-            'es-EU': {},
-            'fa-IR': {},
-            'fi-FI': {},
-            'fr-FR': {},
-            'he-IL': {},
-            'hu-HU': {},
-            'id-ID': {},
-            'it-IT': {},
-            'ja-JP': {},
-            'ko-KR': {},
-            'nb-NO': {},
-            'nl-NL': {},
-            'pl-PL': {},
-            'pt-BR': {},
-            'ro-RO': {},
-            'ru-RU': {},
-            'sk-SK': {},
-            'sl-SI': {},
-            'sr-RS': {},
-            'sr-RS-Latin': {},
-            'sv-SE': {},
-            'th-TH': {},
-            'tr-TR': {},
-            'uk-UA': {},
-            'vi-VN': {},
-            'zh-CN': {
-                highlight: {
-                    highlight: '插入代码',
-                    insert: '插入代码',
-                    codeLabel: '输入代码片段'
-                }
-            },
-            'zh-TW': {}
+
+                var $label = $('<label />');
+                $label.html('Select language');
+                $box.append($selectGroup.append($label));
+                $box.append($selectGroup.append($select));
+
+                var $label = $('<label />');
+                $label.html('Enter the code fragment');
+                var $textarea = $('<textarea class="ext-highlight-code form-control" rows="10" />');
+
+                $box.append($textGroup.append($label));
+                $box.append($textGroup.append($textarea));
+
+                return $box.html();
+            };
+
+            this.createCodeNode = function (code, select) {
+                var $code = $('<code>');
+                $code.html(code);
+                $code.addClass('language-' + select);
+
+                var $pre = $('<pre>');
+                $pre.html($code)
+                $pre.addClass('prettyprint').addClass('linenums');
+
+                return $pre[0];
+            };
+
+            this.showHighlightDialog = function (codeInfo) {
+                return $.Deferred(function (deferred) {
+                    var $extHighlightCode = self.$dialog.find('.ext-highlight-code');
+                    var $extHighlightBtn = self.$dialog.find('.ext-highlight-btn');
+
+                    ui.onDialogShown(self.$dialog, function () {
+                        context.triggerEvent('dialog.shown');
+
+                        $extHighlightCode.text(codeInfo);
+
+                        $extHighlightCode.on('input', function () {
+                            ui.toggleBtn($extHighlightBtn, $extHighlightCode.val() != '');
+
+                            codeInfo = $extHighlightCode.val();
+                        });
+
+                        $extHighlightBtn.on('click', function (event) {
+                            event.preventDefault();
+                            console.log(self.createCodeNode(codeInfo, 'php'));
+                            context.invoke('editor.insertNode', self.createCodeNode(codeInfo, 'php'));
+
+                            self.$dialog.modal('hide');
+                        });
+                    });
+
+                    ui.onDialogHidden(self.$dialog, function () {
+                        context.triggerEvent('dialog.shown');
+                        deferred.resolve();
+                    });
+                    ui.showDialog(self.$dialog);
+                }).promise();
+            };
+
+            this.getCodeInfo = function () {
+                var text = context.invoke('editor.getSelectedText');
+                return '';
+            };
+
+            this.show = function () {
+                var codeInfo = self.getCodeInfo();
+
+                context.invoke('editor.saveRange');
+                this.showHighlightDialog(codeInfo).then(function (codeInfo) {
+                    context.invoke('editor.restoreRange');
+                });
+            };
+
+            //// This events will be attached when editor is initialized.
+            //this.event = {
+            //    // This will be called after modules are initialized.
+            //    'summernote.init': function (we, e) {
+            //        console.log('summernote initialized', we, e);
+            //    },
+            //    // This will be called when user releases a key on editable.
+            //    'summernote.keyup': function (we, e) {
+            //        console.log('summernote keyup', we, e);
+            //    }
+            //};
+            //
+            //// This method will be called when editor is initialized by $('..').summernote();
+            //// You can create elements for plugin
+            this.initialize = function () {
+                var $container = options.dialogsInBody ? $(document.body) : $editor;
+
+                var body = [
+                    '<button href="#" class="btn btn-primary ext-highlight-btn disabled" disabled>',
+                    'Insert code',
+                    '</button>'
+                ].join('');
+
+                this.$dialog = ui.dialog({
+                    className: 'ext-highlight',
+                    title: 'Insert code',
+                    body: this.createDialog(),
+                    footer: body,
+                    //callback: function ($node) {
+                    //    $node.find('.modal-body').css({
+                    //        'max-height': 300,
+                    //        'overflow': 'scroll'
+                    //    });
+                    //}
+                }).render().appendTo($container);
+            };
+
+            // This methods will be called when editor is destroyed by $('..').summernote('destroy');
+            // You should remove elements on `initialize`.
+            this.destroy = function () {
+                ui.hideDialog(this.$dialog);
+                this.$dialog.remove();
+            };
         }
     });
 }));
